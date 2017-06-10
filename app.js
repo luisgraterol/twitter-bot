5929f3d0a8a42308d3d0d1f3a9e1743943f7bfd8
Luis Alejandro Graterol <luisgraterol97@gmail.com>
2017-06-10 14:28:10 -0400

    Built app from scratch
    Added simple functionality to favorite 10 tweets returned from a search
    of the most recent tweets written in English that contain a specified
    hashtag.

---
+++ app.js
@@ -0,0 +1,34 @@
+var Twitter = require('twitter');
+var config = require('./config.js');
+var T = new Twitter(config);
+
+// Set up your search parameters:
+var params = {
+    q: '#starwars',
+    count: 10,
+    result_type: 'recent',
+    lang: 'en'
+}
+
+T.get('search/tweets', params, function(err, data, response) {
+    if(!err) {
+        // Loop through the return tweets:
+        for(let i = 0; i < data.statuses.length; i++) {
+            // Get the tweet ID from the return data:
+            let id = { id: data.statuses[i].id_str }
+            // Try to favorite the selected tweet:
+            T.post('favorites/create', id, function(err, response) {
+                if(!err) { // If there is no error, log the url of the tweet:
+                    let username = response.user.screen_name;
+                    let tweetId = response.id_str;
+                    console.log("Favorited: ",`https://twitter.com/${username}/status/${tweetId}`)
+                }
+                else { // Else, log the error produced.
+                    console.log(err[0].message);
+                }
+            })
+        }
+    }
+    else
+        console.log(error);
+})
---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  consumer_key: '7aZ2mwMQz5On4oOcgkYDxrCfy',
+  consumer_secret: 'aTqVyFb35ykta6Ro0cLUt6wSmaAJv9kl0hTt5wapmlcAVS9b9R',
+  access_token_key: '269402339-B5oEbuxT5cBwTf4svlPSUYz9S2fFtVEYJ9bskVLd',
+  access_token_secret: '1nvrbkXli4JaHwzfqY7S6Z7F5pdXy4g3PUvz1yq184XUg'
+}
